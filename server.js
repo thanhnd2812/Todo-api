caaae32122c49471c50c294a62832251f8d214c6
Thanh Nguyen <thanh.nguyen@zorpiahq.com>
2017-01-27 11:42:39 +0700

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -20,12 +21,7 @@ app.get('/todos', function(req, res){
 // GET /todos/:id
 app.get('/todos/:id', function(req, res){
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
-	todos.forEach(function(todo){
-		if (todo.id === todoId) {
-			matchedTodo = todo;
-		}
-	});
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 	if (matchedTodo) {
 		res.json(matchedTodo);
 	} else {
@@ -37,6 +33,13 @@ app.get('/todos/:id', function(req, res){
 // POST /todos
 app.post('/todos', function(req, res){
 	var body = req.body;
+	body = _.pick(body, 'description', 'completed');
+	if (!_.isBoolean(body.completed) || 
+		!_.isString(body.description) || 
+		body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+	body.description = body.description.trim();
 	body.id = todoNextId++;
 	todos.push(body);
 	res.json(body);
